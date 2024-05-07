9c7622d268ccddb31a7ff459e589c0c06b02fcb6
sebas17sf <sjflores2@espe.edu.ec>
2024-05-07 11:56:12 -0500

    comit laren

--- resources/views/layouts/registro.blade.php
+++ resources/views/layouts/registro.blade.php
@@ -3,23 +3,28 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/x-icon" href="\img\logos\logo_tesis.png" alt="logo">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <!-- Add Bootstrap CSS link -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="../css/admin/admin.css">
     <link rel="stylesheet" href="../css/estudiante/estudiante.css">
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-
-</head>
-<style>
+    <style>
         body {
             overflow-x: hidden;
         }
     </style>
     <script src="../js/menu.js"></script>
+</head>
+
 <body>
     <!-- Barra de navegación en el lado izquierdo -->
     <section class="content-sidebar " _ngcontent-ng-c4160891441>
@@ -37,7 +42,7 @@
                 <div class="links_site">
                     <nav class="nav">
                         <ul class="nav-list">
-                            <a href="#">
+                            <a href="{{ route('estudiantes.create') }}" class="p-element">
                             <div class="icon-sidebar-item">
                                         <i class="material-icons">check_circle</i>
                                         </div>
@@ -55,49 +60,48 @@
                 </span>
             </div>
     </section>
-    <!-- SIDEBAR -->
-    <section class="content-navbar dimension-nav">
-        <!-- Toggle sidebar -->
-        <div class="icon-menu-sidebar" onclick="triggerToggleSidebar()">
-            <i class='bx bx-menu-alt-left'
-                [ngClass]="{'bx-menu': sidebarHidden,'bx-menu-alt-left': !sidebarHidden}"></i>
-        </div>
-        <!-- contenido -->
-        <main class="navbar">
-            <button class="profile-icon dropdown" id="profile-button">
+   <!-- SIDEBAR -->
+   <section class="content-navbar dimension-nav">
+    <!-- Toggle sidebar -->
+    <div class="icon-menu-sidebar" onclick="triggerToggleSidebar()">
+        <i class='bx bx-menu-alt-left'
+            [ngClass]="{'bx-menu': sidebarHidden,'bx-menu-alt-left': !sidebarHidden}"></i>
+    </div>
+    <!-- contenido -->
+    <main class="navbar">
+        <button class="profile-icon dropdown" id="profile-button">
 
-                <div class="name-profile">
-                    <span><?php echo Auth::user()->NombreUsuario; ?></span>
-                </div>
-                <div class="icon-profile">
-                    <img src="../img/default/user.svg">
-                </div>
-
-            </button>
-            <!-- Aquí agregamos el contenedor del menú desplegable -->
-            <div class="popup-menu-profile">
-                <div class="container">
-                    <a href="#" class="change_module">
-                        <i class="fa-regular fa-rectangle-vertical-history"></i>
-                        <span>Cambiar modulo</span>
-                    </a>
-                    <a class="logout" href="{{ route('logout') }}">
-    <i class="fa-sharp fa-regular fa-arrow-up-left-from-circle fontawesome"></i>
-    <span>Cerrar sesión</span>
-</a>
-
-                </div>
+            <div class="name-profile">
+                <span><?php echo Auth::user()->NombreUsuario; ?></span>
             </div>
-        </main>
+            <div class="icon-profile">
+                <img src="../img/default/user.svg">
+            </div>
+        </button>
+        <!-- Aquí agregamos el contenedor del menú desplegable -->
+        <div class="popup-menu-profile">
+            <div class="container">
+                <a href="#" class="change_module">
+                    <i class="fa-regular fa-rectangle-vertical-history"></i>
+                    <span>Cambiar modulo</span>
+                </a>
+                <a class="logout" href="{{ route('logout') }}">
+                    <i class="fa-sharp fa-regular fa-arrow-up-left-from-circle fontawesome"></i>
+                    <span>Cerrar sesión</span>
+                </a>
 
-    </section>
-    <button id="btn_top" *ngIf="showScrollButton" (click)="scrollToTop()"><i class='bx bxs-chevrons-up'></i></button>
+            </div>
+        </div>
+    </main>
+
+</section>
+<button id="btn_top" *ngIf="showScrollButton" (click)="scrollToTop()"><i class='bx bxs-chevrons-up'></i></button>
 
     <!-- CONTENEDOR -->
     <section class="content-views dimension-content" [ngClass]="{'dimension-content-hidden': sidebarHidden}">
         <!-- Title component -->
         <div class="title-component">
-            <h1>@yield('title_component')</h1>
+            <span class="title-content">@yield('title_component')</span>
             <div class="divisor-title"></div>
         </div>
         <!-- Contenido principal -->
@@ -122,11 +126,11 @@
     <script src="{{ asset('js/plantilla/main.js') }}" type="module"></script>
     <script src="{{ asset('js/admin/general.js') }}"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-<!-- Box Icons -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <!-- Box Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
     integrity="sha384-gL5q2wHNwpg9voDwmz1onh73oSJ8lFvZEydTHpw4M4okQ7N8qI+v5h0zitOykKdp" crossorigin="anonymous">
-<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </body>
 
 </html>
