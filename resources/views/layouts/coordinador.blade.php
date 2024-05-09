14d6157744cedbe0b49309ee929e1427a6e67845
Karen Jeomaira Cueva Perez <kjcueva1@espe.edu.ec>
2024-05-08 21:39:44 -0500

    Estructura general frontend

--- resources/views/layouts/coordinador.blade.php
+++ resources/views/layouts/coordinador.blade.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="hydrated">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="\img\logos\logo_tesis.png" alt="logo">
-
     <title>@yield('title')</title>
-    <!-- Agrega el enlace al archivo CSS de Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- Agrega el enlace a los íconos de Google Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.js"></script>
-
-    <link rel="stylesheet" href="css/admin/admin.css">
-    <script src="js/menu.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="../css/admin/admin.css">
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    <style>
+        body {
+            overflow-x: hidden;
+        }
+    </style>
+    <script src="../js/menu.js"></script>
 </head>
+
 
 <body>
     <!-- Barra de navegación en el lado izquierdo -->
-    <section class="content-sidebar" [ngClass]="{'content-sidebar-hidden': sidebarHidden}">
+    <section class="content-sidebar " _ngcontent-ng-c4160891441>
 
         <div class="content scroll-small">
             <div class="sidebar">
@@ -39,18 +41,14 @@
                 <div class="links_site">
                     <nav class="nav">
                         <ul class="nav-list">
-                            <a class="p-element active-section" href="{{ route('coordinador.index') }}"
-                                data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip"
-                                data-bs-title="Proyectos">
+                            <a class="p-element active-section" href="{{ route('coordinador.index') }}">
                                 <div class="icon-sidebar-item">
                                     <i class="material-icons">assignment</i>
                                 </div>
                                 <div class="name-sidebar-item">
                                     <li>Proyectos</li>
                                 </div>
-                                <a href="{{ route('coordinador.proyectosEstudiantes') }}" class="p-element"
-                                    data-bs-toggle="tooltip" data-bs-placement="right"
-                                    data-bs-custom-class="custom-tooltip" data-bs-title="Proyectos-Estudiantes">
+                                <a class="p-element" href="{{ route('coordinador.proyectosEstudiantes') }}">
                                     <div class="icon-sidebar-item">
                                         <i class="material-icons">assignment_ind</i>
                                     </div>
@@ -58,9 +56,7 @@
                                         <li>Proyectos-Estudiantes</li>
                                     </div>
                                 </a>
-                                <a href="{{ route('coordinador.estudiantesAprobados') }}" class="p-element"
-                                    data-bs-toggle="tooltip" data-bs-placement="right"
-                                    data-bs-custom-class="custom-tooltip" data-bs-title="Estudiantes">
+                                <a class="p-element" href="{{ route('coordinador.estudiantesAprobados') }}" >
                                     <div class="icon-sidebar-item">
                                         <i class="material-icons">people</i>
                                     </div>
@@ -68,19 +64,16 @@
                                         <li>Estudiantes</li>
                                     </div>
                                 </a>
-                                <div class="dropdown">
-                                    <a href="#" class="nav-link dropdown-toggle" id="practicasDropdown"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false">
-                                        <div class="icon-sidebar-item">
-                                            <i class="material-icons">business</i>
-                                        </div>
-                                        <div class="name-sidebar-item">
-                                            <li>Prácticas</li>
-                                        </div>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="practicasDropdown">
-                                        <a class="dropdown-item" href="{{ route('coordinador.agregarEmpresa') }}">
+                                <a class="p-element submenu" class="p-element">
+                                <div class="icon-sidebar-item">
+                                    <i class="material-icons">business</i>
+                                </div>
+                                <div class="name-sidebar-item">
+                                    <li>Prácticas</li>
+                                </div>
+                            </a>
+                                    <div class="item-list" id="sublista">
+                                        <a class="p-element mb-1" href="{{ route('coordinador.agregarEmpresa') }}">
                                             <div class="icon-sidebar-item">
                                                 <i class="material-icons">add_business</i>
                                             </div>
@@ -88,8 +81,8 @@
                                                 <li>Agregar-Empresa</li>
                                             </div>
                                         </a>
-                                        <a class="dropdown-item" href="{{ route('coordinador.aceptarFaseI') }}"
-                                            style="margin-top: 10px;">
+                                        <a class="p-element" href="{{ route('coordinador.aceptarFaseI') }}"
+                                            >
                                             <div class="icon-sidebar-item">
                                                 <i class="material-icons">check_circle</i>
                                             </div>
@@ -104,50 +97,55 @@
                 </div>
             </div>
             <div class="content-autors">
-                <span class="autors">
+                <span class="autors1">
                     <i>Designed by Sebastian Flores & Karen Cueva.</i>
                 </span>
             </div>
         </div>
         </div>
     </section>
-    <!-- SIDEBAR -->
-    <section class="content-navbar dimension-nav" _ngcontent-ng-c3252749989>
+   <!-- SIDEBAR -->
+   <section class="content-navbar dimension-nav">
         <!-- Toggle sidebar -->
         <div class="icon-menu-sidebar" onclick="triggerToggleSidebar()">
             <i class='bx bx-menu-alt-left'
                 [ngClass]="{'bx-menu': sidebarHidden,'bx-menu-alt-left': !sidebarHidden}"></i>
         </div>
         <!-- contenido -->
-
         <main class="navbar">
             <button class="profile-icon dropdown" id="profile-button">
-                <div class="icon-profile">
-                    <i class="material-icons">person</i>
-                </div>
+
                 <div class="name-profile">
                     <span><?php echo Auth::user()->NombreUsuario; ?></span>
+                </div>
+                <div class="icon-profile">
+                    <img src="../img/default/user.svg">
                 </div>
             </button>
             <!-- Aquí agregamos el contenedor del menú desplegable -->
             <div class="popup-menu-profile">
                 <div class="container">
+                    <a href="#" class="change_module">
+                        <i class="fa-regular fa-rectangle-vertical-history"></i>
+                        <span>Cambiar modulo</span>
+                    </a>
                     <a class="logout" href="{{ route('logout') }}">
-                        <i class="material-icons">exit_to_app</i>
+                        <i class="fa-sharp fa-regular fa-arrow-up-left-from-circle fontawesome"></i>
                         <span>Cerrar sesión</span>
                     </a>
+
                 </div>
             </div>
         </main>
 
     </section>
-    <button id="btn_top" *ngIf="showScrollButton" (click)="scrollToTop()"><i
-            class='bx bxs-chevrons-up'></i></button>
+    <button id="btn_top" *ngIf="showScrollButton" (click)="scrollToTop()"><i class='bx bxs-chevrons-up'></i></button>
+
     <!-- CONTENEDOR -->
-    <section class="content-views dimension-content" _ngcontent-ng-c3252749989>
+    <section class="content-views dimension-content">
         <!-- Title component -->
         <div class="title-component">
-            <h1>@yield('title_component')</h1>
+        <span class="title-content">@yield('title_component')</span>
             <div class="divisor-title"></div>
         </div>
         <!-- Contenido principal -->
@@ -155,18 +153,31 @@
             <!-- Contenido específico de la página -->
             @yield('content')
         </div>
+        {{--   <button id="btn_top" ><i class='bx bxs-chevrons-up'></i></button> --}}
+
     </section>
 
+    <!-- Scripts de jQuery y Popper.js -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
-    <script src="{{ asset('js/plantilla/styles.js') }}" defer></script>
+    <!-- Script de Bootstrap 4.5.2 -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Script de Bootstrap 5.3.0 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('js/plantilla/styles.js') }}" type="module"></script>
     <script src="{{ asset('js/plantilla/vendor.js') }}" type="module"></script>
     <script src="{{ asset('js/plantilla/main.js') }}" type="module"></script>
-
+    <script src="{{ asset('js/admin/general.js') }}"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
     <!-- Box Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha384-gL5q2wHNwpg9voDwmz1onh73oSJ8lFvZEydTHpw4M4okQ7N8qI+v5h0zitOykKdp" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </body>
 
