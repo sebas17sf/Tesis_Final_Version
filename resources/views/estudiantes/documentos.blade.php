81a90ca8da826f75d90a3d674339b8eb56856e9f
Karenjeo <kjcueva1@espe.edu.ec>
2024-05-20 23:47:33 -0500

    iconos en estudiantes

--- resources/views/estudiantes/documentos.blade.php
+++ resources/views/estudiantes/documentos.blade.php
@@ -362,6 +362,6 @@
     <script src="{{ asset('js/documentosEstudiantes.js') }}"></script>
 
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/fontawesome.min.css">
+{{--     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/fontawesome.min.css"> --}}
 @endsection
