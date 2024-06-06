8436b24cd4c73a071e223005b6efad107cdd5e6d
Karen Jeomaira Cueva Perez <kjcueva1@espe.edu.ec>
2024-06-06 05:18:59 -0500

    update

--- resources/views/admin/indexProyectos.blade.php
+++ resources/views/admin/indexProyectos.blade.php
@@ -660,9 +660,7 @@
     <script src="https://cdn.ckeditor.com/4.16.1/standard/ckeditor.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="js\admin\acciones.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
