8a82eb6b6ffd1d79be2a8a40387079a5e74b909d
sebas17sf <sjflores2@espe.edu.ec>
2024-06-09 20:01:39 -0500

    cambio de bd

---
+++ database/migrations/2023_09_07_161532_nrc.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up(): void
+    {
+        Schema::create('nrc', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idPeriodo');
+            $table->string('nrc');
+            $table->string('tipo');
+            $table->timestamps();
+
+             $table->foreign('idPeriodo')->references('id')->on('periodo');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('nrc');
+    }
+};
