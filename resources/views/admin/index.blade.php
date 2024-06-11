a36ab38bf65ba9bc4c0f383b4328e767c32a44b3
sebas17sf <sjflores2@espe.edu.ec>
2024-06-11 08:31:57 -0500

    cambios

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -181,7 +181,7 @@
         <button type="button" class="close" onclick="document.getElementById('editCard{{ $profesor->id }}').style.display='none'">&times;</button>
     </div>
     <div class="card-body">
-        <form method="POST" action="{{ route('admin.updateEstado', ['id' => $profesor->userid]) }}">
+        <form method="POST" action="{{ route('admin.updateEstado', ['id' => $profesor->userId]) }}">
             @csrf
             @method('PUT')
             <div class="form-group">
@@ -741,7 +741,7 @@
                                         </div>
                                     </form>
                                 </div>
-                            </div>  
+                            </div>
                 <!-- ----------------------------------------------------------------------------------- -->
 
                 <!-- Tarjeta movible para Editar Periodo -->
