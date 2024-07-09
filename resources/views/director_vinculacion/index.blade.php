8e00f6572cc4597e7e74f74be72f9a7ec7393a9c
sebas17sf <sjflores2@espe.edu.ec>
2024-07-08 21:50:09 -0500

    coreccion

--- resources/views/director_vinculacion/index.blade.php
+++ resources/views/director_vinculacion/index.blade.php
@@ -81,13 +81,14 @@
                                     <tr
                                         class="mat-mdc-header-row mdc-data-table__header-row cdk-header-row ng-star-inserted">
                                         <th class="tamanio">NOMBRE DEL PROYECTO</th>
+                                        <th class="tamanio">CODIGO DE PROYECTO</th>
                                         <th class="tamanio1">DIRECTOR</th>
                                         <th class="tamanio">ACTIVIDADES A REALIZAR</th>
                                         <th>CORREO</th>
                                         <th>DEPARTAMENTO</th>
                                         <th>FECHA INICIO</th>
                                         <th>FECHA FIN</th>
-                                        <th>PERIODO</th>
+                                        <th>ESTADO</th>
                                     </tr>
                                 </thead>
                                 <tbody class="mdc-data-table__content ng-star-inserted">
@@ -101,6 +102,7 @@
                                             <tr>
                                                 <td style=" text-transform: uppercase; word-wrap: break-word; text-align: justify;">
                                                     {{ strtoupper($proyecto->nombreProyecto) }}</td>
+                                                <td>{{ $proyecto->codigoProyecto }}</td>
                                                 <td style=" text-transform: uppercase; word-wrap: break-word; text-align: left;">{{ strtoupper($proyecto->director->apellidos) }}
                                                     {{ strtoupper($proyecto->director->nombres) }}</td>
                                                 <td style=" text-transform: uppercase; word-wrap: break-word; text-align: justify;">
@@ -109,11 +111,10 @@
                                                 <td >{{ $proyecto->director->correo }}</td>
 
                                                 <td style=" text-transform: uppercase; word-wrap: break-word; text-align: left;">{{ $proyecto->departamentoTutor }}</td>
-                                                <td>{{ optional($proyecto->asignaciones->first())->inicioFecha ?? 'Fecha no disponible' }}</td>
-                                                <td>{{ optional($proyecto->asignaciones->first())->finalizacionFecha ?? 'Fecha no disponible' }}</td>
-                                                @foreach ($proyecto->asignaciones as $asignacion)
-                                                    <td>{{ $asignacion->periodo->numeroPeriodo }}</td>
-                                                @endforeach
+                                                <td>{{ $proyecto->inicioFecha ?? 'Fecha no disponible' }}</td>
+                                                <td>{{ $proyecto->finFecha ?? 'Fecha no disponible' }}</td>
+                                                <td style=" text-transform: uppercase; word-wrap: break-word; text-align: left;">{{ $proyecto->estado }}</td>
+
                                             </tr>
                                         @endforeach
                                     @endif
